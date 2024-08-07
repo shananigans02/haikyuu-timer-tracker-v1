096c825157423b95a638d71d6112223993b33092
shananigans <shana.ahemode@gmail.com>
2024-08-06 22:53:24 -0700

    fixed format time again

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function logPomodoro(duration) {
     const formatTime = (date) => {
         let hours = date.getHours();
         const mins = date.getMinutes();
-        const ampm = hours > 12 ? 'PM': 'AM';
+        const ampm = hours >= 12 ? 'PM': 'AM';
         
         hours = hours % 12;
         hours = hours ? hours : 12;
